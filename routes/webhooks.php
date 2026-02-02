3064e4c39035cb602d06f0b15e9900a0122f8c2b
Nahuel <nahuefer173@gmail.com>
2026-02-02 20:19:51 -0300

    chore: group webhook routes by middleware

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -4,16 +4,18 @@ use App\Http\Controllers\Webhook\TaskRouterController;
 use App\Http\Controllers\Webhook\TwilioController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('twilio/voice')->middleware('twilio.signature')->name('twilio.voice.')->group(function () {
-    Route::post('/incoming', [TwilioController::class, 'incoming']);
-    Route::post('/outbound', [TwilioController::class, 'outbound']);
+Route::middleware('twilio.signature')->prefix('twilio')->group(function () {
+    Route::prefix('voice')->name('twilio.voice.')->group(function () {
+        Route::post('/incoming', [TwilioController::class, 'incoming']);
+        Route::post('/outbound', [TwilioController::class, 'outbound']);
 
-    Route::post('/handle-input', [TwilioController::class, 'handleInput'])->name('handle-input');
-    Route::post('/dial-complete', [TwilioController::class, 'dialComplete'])->name('dial-complete');
-    Route::post('/queue-status', [TwilioController::class, 'queueStatus'])->name('queue-status');
-});
+        Route::post('/handle-input', [TwilioController::class, 'handleInput'])->name('handle-input');
+        Route::post('/dial-complete', [TwilioController::class, 'dialComplete'])->name('dial-complete');
+        Route::post('/queue-status', [TwilioController::class, 'queueStatus'])->name('queue-status');
+    });
 
-Route::prefix('twilio/taskrouter')->middleware('twilio.signature')->group(function () {
-    Route::post('/assignment', [TaskRouterController::class, 'assignment']);
-    Route::post('/events', [TaskRouterController::class, 'events']);
+    Route::prefix('taskrouter')->group(function () {
+        Route::post('/assignment', [TaskRouterController::class, 'assignment']);
+        Route::post('/events', [TaskRouterController::class, 'events']);
+    });
 });
